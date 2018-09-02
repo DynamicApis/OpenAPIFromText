62b4a4c00074dfa0fa718c0fe76c947dd0d58ecb
DAPI-JBrown <jason@dynamicapis.com>
2018-09-02 15:41:50 +0000

    got the right syntax for building and running with dependencies

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -jar ./target/openapift.jar -codecomments true -input ./testdata/recursivetest -output openapidoc.json
+java -jar ./target/openapift.jar -codecomments true -input ./testdata/recursivetest -output ./openapidoc.json
